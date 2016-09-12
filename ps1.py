df1733f406a5272a5713c8ad8e108cc83d422fd9
2016JTM2084 <mahendrabhadoria892@gmail.com>
2016-09-12 13:55:16 +0530

    first commit

--- ps1.py
+++ ps1.py
@@ -1,3 +1,16 @@
 ###### this is the first .py file ###########
 
 ####### write your code here ##########
+import sys
+
+a=list(sys.argv)
+l=range(1,len(a))
+for i in range(1,len(a)):
+	c=0
+	for j in range(1,len(a)):
+		if a[i]==a[j]:
+			c=c+1
+	l[i-1]=c	
+	print "%s occurs %d times in the string" % (a[i], c)
+
+print l
